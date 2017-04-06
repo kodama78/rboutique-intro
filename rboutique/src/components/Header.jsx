5482d164e180e418d3872e741a65f77add67687c
Trevor Linan <Trevor@Trevors-MacBook-Pro.local>
2017-04-06 14:25:52 -0700

    update component syntax

---
+++ rboutique/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => (
+  <div className="header">
+    <ul>
+      <li>Welcome</li>
+      <li>Our Macarons</li>
+      <li>Gifts & Parties</li>
+      <li>Contact</li>
+    </ul>
+  </div>
+);
+
+
+export default Navbar;
