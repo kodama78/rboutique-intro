16f6f5cd848fda52ca57f17d4ab870d320a21862
sotomo-insikt <sotomo@insikt.com>
2017-04-06 15:02:10 -0700

    started on header css

--- rboutique/src/components/Header.jsx
+++ rboutique/src/components/Header.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import Logo from '../assets/images/logo.png';
 
 const Navbar = () => {
     return (
       <div className="header">
-          <ul>
-              <li>Welcome</li>
-              <li>Our Macarons</li>
-              <li>Gifts & Parties</li>
-              <li>Contact</li>
-          </ul>
+          <div className="logo-ctr">
+              <img src={ Logo } alt=""/>
+          </div>
+          <div className="navbar">
+              <ul>
+                  <li><a href="#">Welcome</a></li>
+                  <li><a href="#">Our Macarons</a></li>
+                  <li><a href="#">Gifts & Parties</a></li>
+                  <li><a href="#">Contact</a></li>
+              </ul>
+          </div>
+
       </div>
     );
 };
