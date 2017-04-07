ad0e26ebd5afab46df276c235c6e0cdb9bfa8964
sotomo-insikt <sotomo@insikt.com>
2017-04-07 15:06:03 -0700

    styling for footer

--- rboutique/src/components/Footer.jsx
+++ rboutique/src/components/Footer.jsx
@@ -1,13 +1,45 @@
 import React from 'react';
+import Mail from '../assets/images/mail.png';
+import Phone from '../assets/images/phone.png';
+import Facebook from '../assets/images/facebook.png';
+import Twitter from '../assets/images/twitter.png';
 
 const Footer = props => (
     <div className="footer">
-      <div className="info-ctr">Email</div>
       <div className="info-ctr">
-          <div className="phone">phone</div>
-          <div className="copyright">Copyright</div>
+          <div className="img-ctr">
+              <img src={ Mail } alt=""/>
+          </div>
+          <div className="text-ctr">
+              <a href="mailto:order@mboutique.com">
+                  order@mboutique.com
+              </a>
+          </div>
       </div>
-      <div className="info-ctr">Follow Us</div>
+      <div className="info-ctr">
+          <div className="img-ctr">
+              <img src={ Phone } alt=""/>
+          </div>
+          <div className="text-ctr">
+              <a href="tel:949-800-3111">
+                  (949) 800-3111
+              </a>
+          </div>
+          <div className="copyright">
+              Copyright Mboutique. All rights reserved.
+          </div>
+      </div>
+      <div className="info-ctr">
+          <div className="text-ctr">
+              Follow us
+          </div>
+          <div className="img-ctr">
+              <img src={ Facebook } alt=""/>
+          </div>
+          <div className="img-ctr">
+              <img src={ Twitter } alt=""/>
+          </div>
+      </div>
     </div>
 );
 
