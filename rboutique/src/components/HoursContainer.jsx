459b13efc79b06a761ed02f86c3cda1994d6d0ca
Trevor Linan <Trevor@Trevors-MacBook-Pro.local>
2017-04-07 14:56:49 -0700

    build and style Hours components

--- rboutique/src/components/HoursContainer.jsx
+++ rboutique/src/components/HoursContainer.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Hours from './Hours';
+import { data } from '../data';
 
-const HoursContainer = props => (
-  <div className="hours-container">
-    <Hours />
-  </div>
-);
+class HoursContainer extends Component {
+  hoursList = () => {
+    return data.map(hours => {
+      return <Hours {...hours} key={hours.weekday} />
+    });
+  }
+
+  render () {
+    return (
+      <div className="hours-container">
+        { this.hoursList() }
+      </div>
+    );
+  }
+}
 
 
 export default HoursContainer;
